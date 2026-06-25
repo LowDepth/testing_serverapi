8b95be92a8d50d4cc6cef4706d96ef685d6b83f0
LowDepth <marcelobts359@gmail.com>
2026-06-25 15:40:26 -0300

    fixed path

--- index.js
+++ index.js
@@ -79,6 +79,21 @@ app.post("/api/notes", (request, response) => {
   response.json(note);
 });
 
+app.put("/api/notes/:id", (request, response) => {
+  const id = request.params.id;
+  const body = request.body;
+
+  const note = notes.find((note) => note.id === id);
+  if (!note) {
+    return response.status(404).json({ error: "note not found" });
+  }
+
+  const updatedNote = { ...note, important: body.important };
+  notes = notes.map((n) => (n.id === id ? updatedNote : n));
+
+  response.json(updatedNote);
+});
+
 app.delete("/api/notes/:id", (request, response) => {
   const id = request.params.id;
   notes = notes.filter((note) => note.id !== id);
